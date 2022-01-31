b940296dc1d4dd54c2c59f1de45f7f14eb93ee5f
AshwiniBhirudharaju <98304623+AshwiniBhirudharaju@users.noreply.github.com>
2022-01-31 20:32:41 +0800

    Add files via upload

---
+++ Module2/forEachIterator.js
@@ -0,0 +1,12 @@
+import { LightningElement } from 'lwc';
+
+export default class ForEachIterator extends LightningElement {
+
+    contacts=[  
+        {id:'c1',Name:'John Abraham',Email:'ja@gmail.com'},
+        {id:'c2',Name:'Brad Pitt',Email:'bpitt@gmail.com'},
+        {id:'c3',Name:'Angelina Jolie',Email:'angelj@gmail.com'},
+        {id:'c4',Name:'Peter Parkar',Email:'spiderman@marvelcomics.com'}
+
+    ];
+}
